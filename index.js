77d9cf06095803575feaecb524e2cb46026019fc
Mohamed <mohamedabuali97@gmail.com>
2025-12-28 22:48:40 -0600

    refactor(index.js): remove commented http server code
    Clean up unused code to improve readability and maintainability. The functionality has been replaced with socket.io implementation.

--- index.js
+++ index.js
@@ -1,44 +1,6 @@
 // const http = require("http")
 import {Server}  from "socket.io"
 
-// const server = http.createServer((request,response) => {
-//       response.setHeader('Access-Control-Allow-Origin', '*');
-//     response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
-//     response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     console.log("url:" + request.url,"method:"+request.method)
-
-//     if (request.method === 'OPTIONS') {
-//         response.writeHead(204);
-//         response.end();
-//         return;
-//     }
-
-//     if(request.url === "/send" && request.method === "POST"){
-
-        
-//         let body = "";
-//         request.on("data",chunk => {
-//             body += chunk.toString();
-//         })
-//         request.on("end", () => {
-//             try{
-//                 console.log(body)
-//             const data  = JSON.parse(body)
-//         const { senderId, receiverId, message } = data;
-
-//         console.log(senderId, receiverId, message);
-//                 response.writeHead(200, { "Content-Type": "application/json" });
-//         response.end(JSON.stringify({ success: true }));
-//       } catch (err) {
-//         response.writeHead(400);
-//         response.end("Invalid JSON");
-//       }
-//         })
-//     }else {
-//     response.writeHead(404);
-//     response.end("Not Found");
-//   }
-// })
 
 
 
